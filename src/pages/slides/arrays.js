a6a68ede9dd261ac234838accb5f2c50f992111a
appenmaier <47243617+appenmaier@users.noreply.github.com>
2022-11-02 12:14:49 +0100

    Update arrays.js

--- src/pages/slides/arrays.js
+++ src/pages/slides/arrays.js
@@ -6,16 +6,10 @@ export default function Arrays() {
   return (
     <Slides initSlides={defaultInitSlides}>
       <section>
-        <div data-block-type="code" style={{ width: auto, height: auto }}>
-          <div data-highlight-theme="darcula" data-code-frame="none" data-code-autoheight="true">
-            <pre style={{ font-size: 16px, line-height: 19px }} class="java">
-              <code data-line-numbers="">
-                Random rnd = new Random();
-                int i1, i2, i3,...i100;
-              </code>
-            </pre>
-          </div>
-        </div>
+        <pre><code data-trim data-noescape data-line-numbers=""><script type="text/template">
+          Random rnd = new Random();
+          int i1, i2, i3,...i100;
+        </script></code></pre>
       </section>
     </Slides>
   );
