d8e49c3c96af3568e9f276b35593f60700e5efa1
appenmaier <47243617+appenmaier@users.noreply.github.com>
2022-11-02 15:19:25 +0100

    Array slides (#48)
    * Update arrays.js
    
    * Add files via upload
    
    * Update index.md

--- src/pages/slides/arrays.js
+++ src/pages/slides/arrays.js
@@ -6,9 +6,14 @@ export default function Arrays() {
   return (
     <Slides initSlides={defaultInitSlides}>
       <section>
+        <div>i1</div>
+        <div>i2</div>
+        <div>i3</div>
+        <div>...</div>
+        <div>i100</div>
         <pre><code data-trim data-noescape data-line-numbers=""><script type="text/template">
-          Random rnd = new Random();
-          int i1, i2, i3,...i100;
+Random rnd = new Random();
+int i1, i2, i3,...i100;
         </script></code></pre>
       </section>
     </Slides>
